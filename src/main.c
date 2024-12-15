d5312c8fbb52c349fb9a0f979ad6774fbe2fe5f5
Bilal Nhari <billelx11@hotmail.fr>
2024-12-15 13:45:43 +0100

    Fix: the flag -h

--- src/main.c
+++ src/main.c
@@ -10,14 +10,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void display_help(void)
+{
+    printf("USAGE: ./myftp port\n");
+    printf("       port is the port number on which the server socket listens.\n");
+}
+
 int main(int ac, char **av)
 {
-    if (error_input(ac, av) == 84)
+    if (ac != 2) {
+        fprintf(stderr, "Invalid argument. Try ./myftp -h\n");
         return 84;
+    }
+    if (strcmp(av[1], "-h") == 0) {
+        display_help();
+        return 0;
+    }
+    if (atoi(av[1]) <= 0) {
+        fprintf(stderr, "Invalid port number. Try ./myftp -h\n");
+        return 84;
+    }
+
     var_t var;
     var.s_sock = create_s_socket(atoi(av[1]));
     if (error_handling(&var) == 84)
         return 84;
+
     init_all_value(&var);
     while (1) {
         init_fd(&var);
